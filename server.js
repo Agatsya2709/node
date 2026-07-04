cb92759d954f8b35db9c078877c816ce716ac95e
Agatsya2709 <136240238+Agatsya2709@users.noreply.github.com>
2026-07-05 01:34:51 +0530

    commit

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import http from "node:http";
-
+import {getdatafromdb} from "./db.js";
 const PORT = 8000;
 
-const server = http.createServer((req, res) => {
-  res.write('this is some data \n')
-  res.write('this is some data \n')
-  res.end("Hello from the server",'utf8', ()=> console.log('response end'));
+const server = http.createServer(async (req, res) => {
+  const destination = await getdatafromdb();
+  //res.write('this is some data \n')
+  //res.write('this is some data \n')
+   if (req.url === 'api'&& req.method === 'GET'){
+  res.end(JSON.stringify(destination), 'utf8', () => console.log('response end'))};
 });
 
 server.listen(PORT, () => {
