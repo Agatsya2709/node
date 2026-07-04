1dfc7e0b31210b4bb68a4ea5b081c760bad88673
Agatsya2709 <136240238+Agatsya2709@users.noreply.github.com>
2026-07-05 01:47:21 +0530

    commit

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const server = http.createServer(async (req, res) => {
   const destination = await getdatafromdb();
   //res.write('this is some data \n')
   //res.write('this is some data \n')
-   if (req.url === 'api'&& req.method === 'GET'){
-  res.end(JSON.stringify(destination), 'utf8', () => console.log('response end'))};
+   if (req.url === '/api'&& req.method === 'GET'){
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode=200;
+  res.end(JSON.stringify(destination), 'utf8', () => console.log('response end'))}else{
+    res.statusCode=404;
+    res.end(JSON.stringify({message: 'not found'}), 'utf8', () => console.log('response end'))
+  };
 });
 
 server.listen(PORT, () => {
