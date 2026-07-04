539bc4a79eee403beb040a66ab31e5d1409e20f6
Agatsya2709 <136240238+Agatsya2709@users.noreply.github.com>
2026-07-05 02:01:08 +0530

    commit

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ const server = http.createServer(async (req, res) => {
    if (req.url === '/api'&& req.method === 'GET'){
     res.setHeader('Content-Type', 'application/json');
     res.statusCode=200;
-  res.end(JSON.stringify(destination), 'utf8', () => console.log('response end'))}else{
+  res.end(JSON.stringify(destination), 'utf8', () => console.log('response end'))}
+  else if(req.url.startsWith('/api/continent/')&& req.method === 'GET' ){
+    const continent = req.url.split('/').pop();
+    const filteredData = destination.filter(item => item.continent.toLowerCase() === continent.toLowerCase());
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode=200;
+    res.end(JSON.stringify(filteredData), 'utf8', () => console.log('response end'))
+  }
+  else{
     res.statusCode=404;
     res.end(JSON.stringify({message: 'not found'}), 'utf8', () => console.log('response end'))
   };
